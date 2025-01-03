75ee584ade6dba4aa0418012820057c6af9ba521
joaodanielcs <joaodanielcs@gmail.com>
2025-01-03 00:02:03 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+apt update
+apt upgrade -y
+timedatectl set-timezone America/Sao_Paulo
+apt install sudo
+sudo apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt install docker-compose
+mkdir nginx && cd nginx
+cat <<EOF > docker-compose.yml
+version: '3'
+services:
+  app:
+    image: 'jc21/nginx-proxy-manager:latest'
+    ports:
+      - '90:80'
+      - '81:81'
+      - '450:443'
+    environment:
+      DB_MYSQL_HOST: "db"
+      DB_MYSQL_PORT: 3306
+      DB_MYSQL_USER: "npm"
+      DB_MYSQL_PASSWORD: "npm"
+      DB_MYSQL_NAME: "npm"
+    volumes:
+      - /srv/dev-disk-by-label-data/dockerapp/nginx/data:/data
+      - /srv/dev-disk-by-label-data/dockerapp/nginx/letsencrypt:/etc/letsencrypt
+  db:
+    image: 'yobasystems/alpine-mariadb'
+    environment:
+      MYSQL_ROOT_PASSWORD: 'npm'
+      MYSQL_DATABASE: 'npm'
+      MYSQL_USER: 'npm'
+      MYSQL_PASSWORD: 'npm'
+    volumes:
+      - /srv/dev-disk-by-label-data/dockerapp/nginx/mysql:/var/lib/mysql
+EOF
+docker-compose up -d
+clear
+ip=$(hostname -I | awk '{print $1}')
+echo "Server:   http://$ip:81/login"
+echo "Username: admin@example.com"
+echo "Password: changeme"
+echo
+echo
