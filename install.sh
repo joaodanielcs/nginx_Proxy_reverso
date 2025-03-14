b17a8179e0c192d26a42493cb9ed71fd77a694d6
joaodanielcs <joaodanielcs@gmail.com>
2025-03-14 19:10:52 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 apt update
 apt upgrade -y
 timedatectl set-timezone America/Sao_Paulo
-apt install sudo
-sudo apt install ca-certificates curl
+apt install sudo -y
+sudo apt install ca-certificates curl -y
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -13,8 +13,8 @@ echo \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo apt install docker-compose
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo apt install docker-compose -y
 mkdir nginx && cd nginx
 cat <<EOF > docker-compose.yml
 version: '3'
